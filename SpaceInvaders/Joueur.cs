d86d93e85ec28a3dc3403167dda4e32a74f9b6ec
Marvin.Fromenteau <marvin.fromenteau@gmail.com>
2017-10-25 09:43:35 +0200

    First commit

---
+++ SpaceInvaders/Joueur.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class Joueur
+    {
+        private static string nom;
+        private static string prenom;
+        private string pseudo;
+
+        public Joueur(string pNom, string pPrenom, string pPseudo)
+        {
+            nom = pNom;
+            prenom = pPrenom;
+            pseudo = pPseudo;
+            formateName();
+        }
+
+        private static void formateName()
+        {
+            nom = nom.ToLower();
+            nom = nom.First().ToString().ToUpper() + nom.Substring(1);
+            prenom = prenom.ToLower();
+            prenom = prenom.First().ToString().ToUpper() + nom.Substring(1);
+        }
+
+        public override string ToString()
+        {
+            return (nom + " " + prenom);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if(obj is Joueur)
+            {
+                return ((Joueur)obj).pseudo == this.pseudo;
+            }
+            return false;
+        }
+        
+    }
+}
---
+++ SpaceInvaders/SpaceInvaders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class SpaceInvaders
+    {
+
+        public SpaceInvaders()
+        {
+            init();
+        }
+
+        void main()
+        {
+            SpaceInvaders si = new SpaceInvaders();
+
+        }
+
+        private void init()
+        {
+            Joueur player1 = new Joueur("aa", "aaa", "aaaa");
+            Joueur player2 = new Joueur("bb", "bbb", "bbbb");
+            Joueur player3 = new Joueur("cc", "ccc", "cccc");
+        }
+    }
+}
