cbbda4e582133844a501a749ad7454b401163f63
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2017-11-07 09:55:47 +0100

    Ajout Interface

---
+++ SpaceInvaders/IAptitude.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    interface IAptitude
+    {
+
+        List<Vaisseau> Utilise(List<Vaisseau> listVaisseau);
+    }
+}
