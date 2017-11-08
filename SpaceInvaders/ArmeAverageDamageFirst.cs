cc6ec8057cc0836a80fbffb2f1307adcc1e67820
Marvin Fromenteau <marvin-dbz@hotmail.fr>
2017-11-08 11:11:04 +0100

    Degat moyen et meilleure
    Ajout de function qui retourne les armes ayant les meilleures dégats moyen et les meilleures dégats min

---
+++ SpaceInvaders/ArmeAverageDamageFirst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class ArmeAverageDamageFirst : Comparer<Arme>
+    {
+        public override int Compare(Arme x, Arme y)
+        {
+            return x.AverageDamage().CompareTo(y.AverageDamage());
+        }
+    }
+}
